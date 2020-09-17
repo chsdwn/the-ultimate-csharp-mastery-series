1757760cb29387510f04384b37ad9c79730b5eb9
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-17 14:21:25 +0300

    4.12-Demo Reference Types and Value Types

--- Program.cs
+++ Program.cs
@@ -2,29 +2,20 @@
 
 namespace ultimate_csharp_mastery
 {
-    enum ShippingMethod
-    {
-        RegularAirMail = 1,
-        RegisteredAirMail = 2,
-        Express = 3
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var method = ShippingMethod.Express;
-            Console.WriteLine(method); // Express
-            Console.WriteLine((int)method); // 3
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine($"a: {a}, b: {b}"); // a: 10, b: 11;
 
-            var methodId = 3;
-            Console.WriteLine((ShippingMethod)methodId); // Express
-
-            Console.WriteLine(method.ToString()); // Express
-
-            var methodName = "Express";
-            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-            Console.WriteLine(shippingMethod); // Express
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine($"array1[0]: {array1[0]}, array2[0]: {array2[0]}");
+            // array1[0]: 0, array2[0]: 0
         }
     }
 }
