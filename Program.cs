2ccfdf3948260b8994d15f51f9f2453bf818a5f9
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-17 13:37:26 +0300

    3.3-Overflowing

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace ultimate_csharp_mastery
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte number = 255;
+            Console.WriteLine($"byte number: {number}"); // 255
+
+            ++number;
+            Console.WriteLine($"byte number + 1: {number}"); // 0
+
+            checked
+            {
+                byte number2 = 255;
+                Console.WriteLine($"byte number2: {number2}"); // 255
+
+                ++number2;
+                Console.WriteLine($"byte number2 + 1: {number2}");
+                //Unhandled exception. System.OverflowException: Arithmetic operation resulted in an overflow
+            }
         }
     }
 }
