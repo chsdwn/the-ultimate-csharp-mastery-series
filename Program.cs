7ce9577dc324f25e28a35aad0c40001ec67a0e99
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-17 14:26:10 +0300

    4.13-Demo Reference Types and Value Types

--- Program.cs
+++ Program.cs
@@ -2,20 +2,32 @@
 
 namespace ultimate_csharp_mastery
 {
+    class Person
+    {
+        public int Age;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine($"a: {a}, b: {b}"); // a: 10, b: 11;
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number); // 1
 
-            var array1 = new int[3] { 1, 2, 3 };
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine($"array1[0]: {array1[0]}, array2[0]: {array2[0]}");
-            // array1[0]: 0, array2[0]: 0
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age); // 30
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }
