9d6f60749b355bcc687e95e095c6627d3a6c45b3
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-17 13:18:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ultimate_csharp_mastery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
