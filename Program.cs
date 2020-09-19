fec3924cd6b44c1df543f53a66cec94930555a18
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-19 17:04:44 +0300

    8.8-Exercises 3

--- Program.cs
+++ Program.cs
@@ -7,37 +7,29 @@ namespace ultimate_csharp_mastery
     {
         static void Main(string[] args)
         {
-            // Write a program and ask the user to enter a few numbers 
-            // separated by a hyphen. If the user simply presses Enter,
-            // without supplying an input, exit immediately; otherwise,
-            // check to see if there are duplicates. If so, 
-            // display "Duplicate" on the console.
+            // Write a program and ask the user to enter a time 
+            // value in the 24-hour time format (e.g. 19:00).
+            // A valid time should be between 00:00 and 23:59.
+            // If the time is valid, display "Ok"; otherwise, 
+            // display "Invalid Time". If the user doesn't
+            // provide any values, consider it as invalid time.
 
-            Console.Write("Enter numbers: ");
+            Console.Write("Enter a time: ");
             var input = Console.ReadLine();
 
             if (String.IsNullOrEmpty(input) || String.IsNullOrWhiteSpace(input))
-                return;
-            else
             {
-                var numberArray = input.Split('-');
-                var numbers = new List<int>();
-
-                foreach (var num in numberArray)
-                    numbers.Add(Convert.ToInt32(num));
-
-                for (int i = 0; i < numbers.Count; i++)
-                {
-                    var currentNumber = numbers[i];
-                    numbers.Remove(currentNumber);
-
-                    if (numbers.Contains(currentNumber))
-                    {
-                        Console.WriteLine("Duplicate");
-                        break;
-                    }
-                }
+                Console.WriteLine("Invalid Time");
+                return;
             }
+
+            var hour = Convert.ToInt32(input.Split(':')[0]);
+            var minute = Convert.ToInt32(input.Split(':')[1]);
+
+            if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+                Console.WriteLine("Ok");
+            else
+                Console.WriteLine("Invalid Time");
         }
     }
 }
