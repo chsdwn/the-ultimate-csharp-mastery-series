44efeeeda7338402539cc23300677543638925ad
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-19 16:58:23 +0300

    8.8-Exercises 2

--- Program.cs
+++ Program.cs
@@ -8,45 +8,36 @@ namespace ultimate_csharp_mastery
         static void Main(string[] args)
         {
             // Write a program and ask the user to enter a few numbers 
-            // separated by a hyphen. Work out if the numbers are consecutive. 
-            // For example, if the input is "5-6-7-8-9" or "20-19-18-17-16",
-            // display a message: "Consecutive"; otherwise, display "Not Consecutive".
+            // separated by a hyphen. If the user simply presses Enter,
+            // without supplying an input, exit immediately; otherwise,
+            // check to see if there are duplicates. If so, 
+            // display "Duplicate" on the console.
 
-            Console.Write("Enter number: ");
-            var userInput = Console.ReadLine();
-            var numberArray = userInput.Split('-');
+            Console.Write("Enter numbers: ");
+            var input = Console.ReadLine();
 
-            var numbers = new List<int>();
-
-            foreach (var num in numberArray)
-                numbers.Add(Convert.ToInt32(num));
-
-            if (numbers[0] < numbers[1])
-                Console.WriteLine(IsConsecutive(numbers, false) ? "Consecutive" : "Not Consecutive");
-            else if (numbers[0] > numbers[1])
-                Console.WriteLine(IsConsecutive(numbers, true) ? "Consecutive" : "Not Consecutive");
+            if (String.IsNullOrEmpty(input) || String.IsNullOrWhiteSpace(input))
+                return;
             else
-                Console.WriteLine("Not Consecutive");
-        }
-
-        static bool IsConsecutive(List<int> numbers, bool isReverse)
-        {
-            for (int i = 0; i < numbers.Count - 1; i++)
             {
-                if (!isReverse)
-                {
+                var numberArray = input.Split('-');
+                var numbers = new List<int>();
 
-                    if (numbers[i] + 1 != numbers[i + 1])
-                        return false;
-                }
-                else
+                foreach (var num in numberArray)
+                    numbers.Add(Convert.ToInt32(num));
+
+                for (int i = 0; i < numbers.Count; i++)
                 {
-                    if (numbers[i] != numbers[i + 1] + 1)
-                        return false;
+                    var currentNumber = numbers[i];
+                    numbers.Remove(currentNumber);
+
+                    if (numbers.Contains(currentNumber))
+                    {
+                        Console.WriteLine("Duplicate");
+                        break;
+                    }
                 }
             }
-
-            return true;
         }
     }
 }
