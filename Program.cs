9fa8a140142b6b16f0404e39c2947d75630d7b8f
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-19 17:20:05 +0300

    8.8-Exercises 5

--- Program.cs
+++ Program.cs
@@ -7,29 +7,22 @@ namespace ultimate_csharp_mastery
     {
         static void Main(string[] args)
         {
-            // Write a program and ask the user to enter a few words separated by a space.
-            // Use the words to create a variable name with PascalCase. For example, 
-            // if the user types: "number of students", display "NumberOfStudents". 
-            // Make sure that the program is not dependent on the input. So, if the user
-            // types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+            // Write a program and ask the user to enter an English word.
+            // Count the number of vowels (a, e, o, u, i) in the word.
+            // So, if the user enters "inadequate", the program should display 6 on the console.
 
-            Console.Write("Enter a few words: ");
+            var vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+            Console.Write("Enter an English word: ");
             var input = Console.ReadLine();
             input = input.ToLower();
 
-            var words = input.Split(' ');
+            var result = 0;
 
-            for (int i = 0; i < words.Length; i++)
-            {
-                var word = words[i];
-                word = word[0].ToString().ToUpper() + word.Substring(1);
-                words[i] = word;
-            }
-
-            var result = "";
-
-            foreach (var word in words)
-                result += word;
+            foreach (var letter in input)
+                foreach (var vowel in vowels)
+                    if (letter.Equals(vowel))
+                        ++result;
 
             Console.WriteLine(result);
         }
