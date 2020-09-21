56656200cb21e5772c58cc5b5c6ff3cb2870cd0c
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-21 14:36:33 +0300

    2.2-Constructors

--- Program.cs
+++ Program.cs
@@ -8,22 +8,6 @@ namespace ultimate_csharp_mastery
     {
         static void Main(string[] args)
         {
-            // Write a program that reads a text file and displays the longest word in the file.
-            var path = "./exercise9.txt";
-            var content = File.ReadAllText(path);
-            var words = content.Split(' ');
-
-            var longestWordLength = 0;
-            var longestWord = "";
-
-            foreach (var word in words)
-                if (word.Length > longestWordLength)
-                {
-                    longestWordLength = word.Length;
-                    longestWord = word;
-                }
-
-            Console.WriteLine(longestWord);
         }
     }
 }
