e87c03b480bce14578a49f6bea7d4b0899d77667
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-21 13:59:30 +0300

    9.6-Exercise 2

--- Program.cs
+++ Program.cs
@@ -8,12 +8,22 @@ namespace ultimate_csharp_mastery
     {
         static void Main(string[] args)
         {
-            // Write a program that reads a text file and displays the number of words.
+            // Write a program that reads a text file and displays the longest word in the file.
             var path = "./exercise9.txt";
             var content = File.ReadAllText(path);
             var words = content.Split(' ');
 
-            Console.WriteLine(words.Length);
+            var longestWordLength = 0;
+            var longestWord = "";
+
+            foreach (var word in words)
+                if (word.Length > longestWordLength)
+                {
+                    longestWordLength = word.Length;
+                    longestWord = word;
+                }
+
+            Console.WriteLine(longestWord);
         }
     }
 }
