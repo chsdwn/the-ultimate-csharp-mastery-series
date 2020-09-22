19f23aca7d25cd5276298525a64edad86dad9c57
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-22 14:15:13 +0300

    6.4-Interfaces are NOT for Multiple Inheritance

---
+++ MultipleInheritance/UIControl.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MultipleInheritance
+{
+    public class UIControl
+    {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public Position TopLeft { get; set; }
+
+        public virtual void Draw()
+        {
+        }
+
+        public void Focus()
+        {
+            Console.WriteLine("Focused");
+        }
+    }
+}
